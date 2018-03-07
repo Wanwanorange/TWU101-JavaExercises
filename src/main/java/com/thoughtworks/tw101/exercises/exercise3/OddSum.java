70a34f05c3a9f30e6bedb5f09df0282ed648aeb5
wanwanorange <wanchen.y95@gmail.com>
2018-03-07 16:32:48 -0600

    Completed exercise 3

--- src/main/java/com/thoughtworks/tw101/exercises/exercise3/OddSum.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise3/OddSum.java
@@ -3,6 +3,14 @@ package com.thoughtworks.tw101.exercises.exercise3;
 public class OddSum {
 
     public int of(int start, int end) {
-        return -1;
+        int sum = 0, pointer = start;
+
+        while (pointer < end) {
+            if (pointer % 2 != 0) {
+                sum += pointer;
+            }
+            pointer++;
+        }
+        return sum;
     }
 }
