efad686956e45500f1f275008694dc6357352568
wanwanorange <wanchen.y95@gmail.com>
2018-03-07 14:41:07 -0600

    Completed exercise 2

--- src/main/java/com/thoughtworks/tw101/exercises/exercise2/Accumulator.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise2/Accumulator.java
@@ -1,4 +1,13 @@
 package com.thoughtworks.tw101.exercises.exercise2;
 
 public class Accumulator {
+    int counter = 0;
+
+    void increment() {
+        counter++;
+    }
+
+    void total() {
+        System.out.println(counter);
+    }
 }
