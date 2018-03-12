75c7dcd197f650bea873f317ea985d1430d6c1f9
wanwanorange <wanchen.y95@gmail.com>
2018-03-12 09:55:17 -0500

    completed exercise 9

--- src/main/java/com/thoughtworks/tw101/exercises/exercise9/Node.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise9/Node.java
@@ -1,18 +1,53 @@
 package com.thoughtworks.tw101.exercises.exercise9;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Node {
     private String name;
+    private Node left, right;
 
     public Node(String name) {
         this.name = name;
     }
 
     public void add(String nameOfNewNode) {
+        //If new-node is bigger, call add to node's right leaf
+        if (nameOfNewNode.compareTo(name) > 0) {
+            if (right == null) {
+                right = new Node(nameOfNewNode);
+            }
+            else {
+                right.add(nameOfNewNode);
+            }
+        }
+        //If new-node is smaller, call add to node's left leaf
+        else if (nameOfNewNode.compareTo(name) < 0) {
+            if (left == null) {
+                left = new Node(nameOfNewNode);
+            }
+            else {
+                left.add(nameOfNewNode);
+            }
+        }
+
     }
 
     public List<String> names() {
-        return null;
+
+        List<String> nameList = new ArrayList<String>();
+        names(nameList, this);
+        return nameList;
     }
+
+
+    public void names(List<String> currentNames, Node currentNode) {
+        if (currentNode != null) {
+            names(currentNames, currentNode.left);
+            currentNames.add(currentNode.name);
+            names(currentNames, currentNode.right);
+        }
+    }
+
 }
+
